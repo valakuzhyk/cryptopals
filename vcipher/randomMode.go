67eee9fb97820f602eae5cd7f44a8c2e747c4aa9
Kevin 'V' Valakuzhy <kevin.valakuzhy@gmail.com>
2018-03-24 18:20:03 -0400

    Add code for a random encrypter and a oracle

--- vcipher/randomMode.go
+++ vcipher/randomMode.go
@@ -1,32 +1,92 @@
 package vcipher
 
 import (
+	"crypto/cipher"
 	"crypto/rand"
 	"log"
 	"math/big"
+	"strings"
+
+	"github.com/valakuzhyk/cryptopals/utils"
+
+	"github.com/valakuzhyk/cryptopals/aes"
 )
+
+type EncryptionMode int
+
+const (
+	ECB_MODE EncryptionMode = iota
+	CBC_MODE
+)
+
+// ECBvsCBCOracle identifies whether or not the function passed in
+// encrypts using ECB or CBC
+func ECBvsCBCOracle(encrypterFunction func(input []byte) []byte) EncryptionMode {
+	// If we send a bunch of the same character, if this is encrypting in ECB,
+	// the middle blocks would be the same.
+	bytesToInput := []byte(strings.Repeat("A", 100))
+	encryptedBytes := encrypterFunction(bytesToInput)
+	repeats := CountRepeats(string(encryptedBytes), 16)
+	if repeats > 0 {
+		return ECB_MODE
+	}
+	return CBC_MODE
+}
+
+type RandomEncrypter struct {
+	Mode EncryptionMode
+}
+
+// SetEncryptionMode sets what kind of encryption the Encrypt function does.
+func (e RandomEncrypter) SetEncryptionMode(newMode EncryptionMode) {
+	e.Mode = newMode
+}
 
 // RandomEncrypter randomly pads information given and encodes in either
 // ECB or CBC mode.
-func RandomEncrypter(input []byte) {
-	frontBytes := GetRandomBytes(5, 10)
+func (e RandomEncrypter) Encrypt(input []byte) []byte {
+	frontBytes := GetRandomBytesBetween(5, 10)
 	input = append(frontBytes, input...)
 
-	endBytes := GetRandomBytes(5, 10)
+	endBytes := GetRandomBytesBetween(5, 10)
 	input = append(input, endBytes...)
+
+	key := GetRandomBytes(16)
+	aesCipher, err := aes.NewBlockCipher(key)
+	if err != nil {
+		log.Println("Couldn't get block cipher to work", err)
+	}
+
+	var encrypter cipher.BlockMode
+	if e.Mode == ECB_MODE {
+		encrypter = NewECBEncrypter(aesCipher)
+	} else if e.Mode == CBC_MODE {
+		encrypter, err = NewCBCEncrypter(aesCipher, GetRandomBytes(16))
+		if err != nil {
+			log.Fatal("CBC encrypter failed ", err)
+		}
+	}
+	paddedInput := utils.AddPKCS7Padding(string(input), encrypter.BlockSize())
+	output := make([]byte, len(input))
+	encrypter.CryptBlocks(output, []byte(paddedInput))
+	return output
 }
 
 // GetRandomBytes returns a number of random bytes between min and max
-func GetRandomBytes(min, max int) []byte {
+func GetRandomBytesBetween(min, max int) []byte {
 	maxIntSize := big.NewInt(int64(max - min))
 	numByteOffset, err := rand.Int(rand.Reader, maxIntSize)
 	if err != nil {
 		log.Fatal("Issue computing random int: ", err)
 	}
-	numBytes := uint64(min) + numByteOffset.Uint64()
+	numBytes := min + int(numByteOffset.Uint64())
 
+	return GetRandomBytes(numBytes)
+}
+
+func GetRandomBytes(numBytes int) []byte {
 	randBytes := make([]byte, numBytes)
-	_, err = rand.Read(randBytes)
+	_, err := rand.Read(randBytes)
 	if err != nil {
 		log.Fatal("Issue computing random bytes: ", err)
 	}
